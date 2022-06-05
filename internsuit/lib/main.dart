848b9e7195f3d21febb220da04c6d4b4207b3c56
Dagim <daginaruto@gmail.com>
2022-06-05 18:44:58 +0300

    Update main.dart

--- internsuit/lib/main.dart
+++ internsuit/lib/main.dart
@@ -1,115 +1,156 @@
+// import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mynotes/application/application_repository/app_repository.dart';
+import 'package:mynotes/application/blocs/app_bloc.dart';
+import 'package:mynotes/auth/auth_repository.dart';
+import 'package:mynotes/auth/login/data_provide/dataprovider.dart';
+import 'package:mynotes/auth/login/login_bloc.dart';
+import 'package:mynotes/post/blocs/post_bloc.dart';
+import 'package:mynotes/post/blocs/post_state.dart';
+import 'package:mynotes/post/data_provider/post_data_provider.dart';
+import 'package:mynotes/post/post_repository/post_repository.dart';
+import 'package:mynotes/user/blocs/user_bloc.dart';
+import 'package:mynotes/user/data_provider/user_data_provider.dart';
+import 'package:mynotes/user/repository/user_repository.dart';
+import 'package:mynotes/views/application_form.dart';
+import 'package:mynotes/views/companies_post.dart';
+import 'package:mynotes/views/company_registration.dart';
+import 'package:mynotes/views/companylogin.dart';
+import 'package:mynotes/views/login.dart';
+import 'package:go_router/go_router.dart';
+import 'package:mynotes/views/user_registration.dart';
+import 'application/dataprovider/app_provider.dart';
+import 'company/blocs/company_bloc.dart';
+import 'company/data_provider/company_data_provider.dart';
+import 'company/repository/company_repository.dart';
+import 'models/companies_post.dart';
+import 'views/Company_message.dart';
+import 'views/Usermessage.dart';
+import 'views/company_navigation.dart';
+import 'views/company_post.dart';
+import 'views/company_profile.dart';
+import 'views/postform.dart';
+import 'views/reject_or_accept_application.dart';
+import 'views/user_navigation.dart';
+import 'views/user_profile.dart';
+
+
+
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+   @override
+  Widget build(BuildContext context) {
+     return MaterialApp(
+       home:RepositoryProvider(
+         create: (context)=>AuthenticationRepository(Loginprovider()),
+         child: const CompaniesPost(),
+       )
+     );
+    
+
+  }
+  }
+   
+ final _router = GoRouter(
+   routes: [
+     GoRoute(
+       path: '/',
+       builder: (context, state) => const CompanyLoginView(),
+     ),
+      GoRoute(
+       path: '/Login',
+       builder: (context, state) => const LoginView(),
+     ),
+     GoRoute(
+       path: '/SignupView',
+       builder: (context, state) =>const  SignupView(),
+     ),
+     GoRoute(
+       path: '/CompanySignupView',
+       builder: (context, state) =>const  CompanySignupView(),
+     ),
+     GoRoute(
+       path: '/ApplicationForm',
+       builder: (context, state) =>const ApplicationForm(),
+     ),
+     GoRoute(
+       path: '/companies_post',
+       builder: (context, state) =>const CompaniesPost(),
+     ),
+     GoRoute(
+       path: '/Companymessage',
+       builder: (context, state) =>const  Companymessage(),
+     ),
+     GoRoute(
+       path: '/CompanyNavigation',
+       builder: (context, state) =>const  CompanyNavigation(),
+     ),
+     GoRoute(
+       path: '/SingleCompaniesPost',
+       builder: (context, state) =>const  SingleCompaniesPost(),
+     ),
+     GoRoute(
+       path: '/CompanyProfile',
+       builder: (context, state) =>const  CompanyProfile(),
+     ),
+     GoRoute(
+       path: '/PostForm',
+       builder: (context, state) =>const  PostForm(),
+     ),
+     GoRoute(
+       path: '/ApplicationEvaluation',
+       builder: (context, state) =>const  ApplicationEvaluation(),
+     ),
+     GoRoute(
+       path: '/UserNavigation',
+       builder: (context, state) =>const  UserNavigation(),
+     ),
+     GoRoute(
+       path: '/UserProfile',
+       builder: (context, state) =>const  UserProfile(),
+     ),
+     GoRoute(
+       path: '/UserMessage',
+       builder: (context, state) =>const  UserMessage(),
+     ),
+
+   ],
+ );
 
 void main() {
-  runApp(const MyApp());
+ runApp(
+    MultiBlocProvider(
+          providers: [
+            BlocProvider<PostBloc>(
+              create: (context) =>
+            PostBloc(postRepository: PostRepository(PostDataProvider()))),
+            BlocProvider<LoginBloc> (create: (context)=> LoginBloc(authrepo: AuthenticationRepository(Loginprovider())),)  ,
+            BlocProvider<AppBloc> (create: (context)=> AppBloc(appRepository: AppRepository(AppDataProvider())),)   ,
+            BlocProvider<CompanyBloc> (create: (context)=> CompanyBloc(companyRepository: CompanyRepository(CompanyDataProvider())),) , 
+            BlocProvider<UserBloc> (create: (context)=> UserBloc(userRepository: UserRepository(UserDataProvider())),)     
+   
+  
+
+
+
+
+  
+            ],
+          
+          
+      
+     child: MaterialApp.router(
+         routeInformationParser: _router.routeInformationParser,
+         routerDelegate: _router.routerDelegate,
+         
+       ),
+   )
+ );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
