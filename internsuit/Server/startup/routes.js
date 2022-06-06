903bc057037ef8f35fcd7946643149edb7bae738
Ephrem Alemayehu <ephremalemayehu2019@gmail.com>
2022-06-05 18:20:12 -0800

    adjesting routes

---
+++ internsuit/Server/startup/routes.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const regestration = require('../routes/registration')
+const login = require('../routes/login')
+const error = require('../middelware/error')
+const getData = require('../routes/getData')
+const apply = require('../routes/applyform')
+const post = require('../routes/post')
+const cors = require("cors")
+const update=require('../routes/update')
+const delet=require('../routes/delete')
+
+module.exports=function(app){
+    app.use(express.json())
+    app.use(cors())
+    app.use('/reg',regestration)
+    app.use('/login',login)
+    app.use('/get',getData)
+    app.use('/put',update)
+    app.use('/apply',apply)
+    app.use('/post',post)
+    app.use(error)
+    app.use('/delete',delet)
+}
